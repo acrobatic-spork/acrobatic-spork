447bf4fb9f506d7a184a51f8177906a2e090d029
Hack Reactor Students <accounts@hackreactor.com>
2016-02-18 19:48:10 -0800

    Able to retrieve uber user authorization and refresh tokens

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
-import express from 'express';
+const express = require('express');
+const yelp = require('./yelp/yelpController');
+const passport = require('passport');
+const uberStrategy = require('passport-uber');
+const request = require('request');
 
 const app = express();
 
@@ -7,7 +11,33 @@ app.use(express.static(__dirname + '/../public'));
 app.post('/api/users/signup');
 app.post('/api/users/signin');
 app.post('/api/users/signout');
-app.post('/api/fuckit');
+app.post('/api/yelp', yelp);
+
+// passport.use(new uberStrategy({
+//     clientID: 'x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m',
+//     clientSecret: '9ddASgYXll_qHgdq7XxWtV0iG7AQfpAwGFh-sFL0',
+//     callbackURL: "http://127.0.0.1:8080/auth/uber/callback"
+//   },
+//   function(accessToken, refreshToken, profile, done) {
+//     console.log('uber auth response', accessToken, refreshToken, profile, done);
+//     // User.findOrCreate({ uberid: profile.id }, function (err, user) {
+//     //   return done(err, user);
+//     // });
+//   }
+// ));
+
+app.get('/auth/uber', function(req, res, next) {
+  console.log('..............', req.query.code);
+  request.post("https://login.uber.com/oauth/v2/token?code=" + req.query.code + "&redirect_uri=http://localhost:8080/auth/uber&client_id=x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m&client_secret=9ddASgYXll_qHgdq7XxWtV0iG7AQfpAwGFh-sFL0&grant_type=authorization_code", function(err, response) {
+    console.log('-------------------------------', err, response);
+  });
+});
+
+// app.get('/auth/uber/callback', function(req, res) {
+//     console.log('final callback', req);
+//     // Successful authentication, redirect home.
+//     res.redirect('/');
+//   });
 
 
 
--- server/yelp/yelpController.js
+++ server/yelp/yelpController.js
@@ -1,6 +1,6 @@
-var Yelp = require('yelp');
+const Yelp = require('yelp');
 
-var yelp = new Yelp({
+const yelp = new Yelp({
   consumer_key: 'mcOd7miyFhdp6Cz1L-oD9w',
   consumer_secret: 'RXwVPYUZropBN8uT9OpUNfVx-u4',
   token: 'qgA95Qm5YSrXItKRTxcR1iNWERcowxow',
@@ -8,21 +8,20 @@ var yelp = new Yelp({
 });
 
 
+module.exports = (req, res, next) => {
+  console.log('yelp api request', req.body);
 
-
-
-var Yelp = function (req, res, next) {
-
-  var location = req.body.location;
+  // const location = req.body.location;
   // search term
   // $$$$
   // rating
 
   yelp.search({ term: 'food', location: 'Montreal' })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
     yelp.business(data)
-      .then(function(business) {
+      .then(function(business) {  
+    res.json(data);
         console.log(business.location.coordinate);
       });
   })
