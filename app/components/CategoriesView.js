c91828892fe9f14199ebac65e23acb94df48e8ec
Hack Reactor Students <accounts@hackreactor.com>
2016-02-19 19:30:19 -0800

    Add category input view component

--- app/components/CategoriesView.js
+++ app/components/CategoriesView.js
@@ -1,20 +1,19 @@
 import React from 'react';
+import CategoryView from './CategoryView.js';
 
 class Categories extends React.Component {
   constructor(props){
     super(props);
   }
+  
 
   
   render (){
-    var categories = ['Mexican', 'Italian', 'Bars', 'Pizza']
 
+    var categories = ['Mexican', 'Italian', 'Bars', 'Pizza']
     var categoryViews = categories.map( category => 
-        (
-        <div>
-          <div> { category }</div>
-        </div>
-        ))
+                          <CategoryView name={ category }/>
+                        )
     
     return (
       <div>
