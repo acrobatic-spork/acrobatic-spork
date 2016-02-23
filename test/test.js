366ebf2d8b83cdbb24b593779d180c190d1a51dd
Silverhawk <sondra.silverhawk@hush.com>
2016-02-22 16:51:15 -0800

    Set up basic testing

--- test/test.js
+++ test/test.js
@@ -1,2 +1,13 @@
-var expect = require('chai').expect;
+'use strict';
 
+var expect = require('chai').expect;
+import 'babel-polyfill';
+
+import User from '../server/users/user';
+import Controller from '../server/users/userController';
+
+describe('the User Controller', function () {
+  it('should have all the functions', function () {
+    expect(Controller.signin).to.be.a('function');
+  });
+});
