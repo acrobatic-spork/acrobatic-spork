0c4ca0706d3264f6685b4a7ed754de856063c1b9
Gerrit <gerrityntema@gmail.com>
2016-02-23 15:19:12 -0800

    Add prefs update endpoint and database call

--- server/uber/uberController.js
+++ server/uber/uberController.js
@@ -10,6 +10,7 @@ module.exports = function(userController) {
 var Uber = {};
 
 Uber.getToken = function (req, res, next) {
+  console.log('>>>>>>>>>>>>>>>>>>>>', req);
   request.post("https://login.uber.com/oauth/v2/token?code=" + req.query.code + "&redirect_uri=http://localhost:8080/auth/uber&client_id=x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m&client_secret=9ddASgYXll_qHgdq7XxWtV0iG7AQfpAwGFh-sFL0&grant_type=authorization_code", function(err, response) {
     console.log('-------------------------------', err, response.body);
     // TODO: Add to database
