f568c551d7456658077da6f91df57cc9b2ef6574
Silverhawk <sondra.silverhawk@hush.com>
2016-02-22 18:20:20 -0800

    Add handler function, pseudocode

--- app/components/Signin.js
+++ app/components/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import function that will send post request to DB
 
 class Signin extends React.Component {
 
@@ -6,20 +7,28 @@ class Signin extends React.Component {
     super(props);
   }
 
-  handleSubmit (e) {
+  handleSignin (e) {
     e.preventDefault();
-    this.props.onSubmit(this.refs.username.value, this.refs.password.value);
+    //confirmUser(this.refs.username.value, this.refs.password.value);
+  }
+
+  handleSignup (e) {
+    e.preventDefault();
+    //addUser(this.refs.username.value, this.refs.password.value);
   }
 
   render() {
 
     return (
       <div className="">
-        <form className="loginform" onSubmit={this.handleSubmit.bind(this)} >
+        <form className="loginform" >
           <input className="username" type="text" ref="username" name="username" placeholder="username" />
           <input className="passord" type="password" ref="password" name="password" placeholder="password" />
-            <button type="submit" className="">
-              <span className="">Signin/Signup</span>
+          <button className="signin" onClick={this.handleSignin.bind(this)}>
+            <span className="">Signin</span>
+          </button>
+          <button className="signup" onClick={this.handleSignup.bind(this)}>
+            <span className="">Signup</span>
           </button>
         </form>
       </div> 
