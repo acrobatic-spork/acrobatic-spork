8da1f5131f11b8691d9a570aab4abc0eae43bc0d
Silverhawk <sondra.silverhawk@hush.com>
2016-02-22 17:44:02 -0800

    Refactor connect uber view into class

--- app/components/ConnectUberView.js
+++ app/components/ConnectUberView.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-const Category = (props) => (
-    <div> 
-      <button type="submit" className="connect-uber">
-           <a href="https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m">Connect Your Uber Account</a>
-      </button>
-    </div>
-  )
+class Category extends React.Component {
+  constructor(props){
+    super(props);
+  }
 
+  render() {
+    return (
+      <div> 
+        <button type="submit" className="connect-uber">
+             <a href="https://login.uber.com/oauth/v2/authorize?response_type=code&client_id=x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m">Connect Your Uber Account</a>
+        </button>
+      </div>
+    )  
+  }
+}
 export default Category
