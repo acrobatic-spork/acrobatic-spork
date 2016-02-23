2021034e052d5efe6694fc59aae54e06f1b0fd26
Silverhawk <sondra.silverhawk@hush.com>
2016-02-22 18:01:10 -0800

    Make into button and add event handler function

--- app/components/SporkView.js
+++ app/components/SporkView.js
@@ -4,11 +4,16 @@ class Spork extends React.Component {
   constructor(props){
     super(props);
   }
+
+  sporkHandler (e) {
+    e.preventDefault();
+    this.props.onSubmit();
+  }
   
   render (){
     return (
       <div>
-        <div onClick={this.props.sporkHandler}className='spork-button'>SporkButton</div>
+        <button onClick={this.sporkHandler}className='spork-button'>SporkButton</button>
       </div>
       )
   }
