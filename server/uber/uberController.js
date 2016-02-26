7308855aa9eb29e50fab733fbe3fa74e55959a4f
Gerrit <gerrityntema@gmail.com>
2016-02-26 15:17:04 -0800

    Update config in uberController

--- server/uber/uberController.js
+++ server/uber/uberController.js
@@ -1,22 +1,24 @@
-
 var request = require ('request');
 
-var client_id = 'x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m';
-var client_secret = 'S5gtX14LRlWMs_oo_F1ANa0lVDjw7kkf6Q0f5QPg';
-var server_token = '1C_9lI-9D2E1K3OuFqH9EeayNcoWzAJendBM3BfT';
-var testEndpoint = 'https://sandbox-api.uber.com/v1/requests';
-var productionEndpoint = 'https://api.uber.com/v1/requests';
-var redirectUri = 'http://localhost:3000/auth/uber'
+var config = {
+  client_id: 'x8ZBOGgvve2JHQgOFuR7ib2e2dt_A66m',
+  client_secret: 'S5gtX14LRlWMs_oo_F1ANa0lVDjw7kkf6Q0f5QPg',
+  server_token: '1C_9lI-9D2E1K3OuFqH9EeayNcoWzAJendBM3BfT',
+  endpoint: 'https://api.uber.com/v1/requests'
+}
 
-var testAccessToken = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicmVxdWVzdCJdLCJzdWIiOiJmNTcxODY1Ny0zMWYxLTQ1OTctYTk4ZC0zNmI4NGQwOGU3NzkiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjI4MjBkYTZjLTcxNWQtNDM1Ni1hNGExLTIyYmNjYWQ1NTgwYiIsImV4cCI6MTQ1ODk0NjU3NCwiaWF0IjoxNDU2MzU0NTc0LCJ1YWN0IjoidUhHQm9RWXJqbUtzSHRVbmtYbFJwbjVrRXhHQTc2IiwibmJmIjoxNDU2MzU0NDg0LCJhdWQiOiJ4OFpCT0dndnZlMkpIUWdPRnVSN2liMmUyZHRfQTY2bSJ9.nBh2WRXLQ-p_hWSMnbWC6jXTOURqZzgwemivQ3YyJrKQLGzhvrOqsbqlWwwOhB2dMco9KwV6JNKoZSskMzvPVdt6Ou15RLnyxYgkHpVsrBb-vgdztvDIWj0VUV55cjqX3KiUNgZwH0ndGDKAvAvFS-OILm_yFegCWnt_CteFXPRuN-S-Q-cE4WZDzMtu7FCDSdPiQC0o83cw9Owf7C_01TnKidpMLY_JTYIaYio_bfdbhQy5MwIttHAbSwrltK8s2lnvBh-qGMnV5Og6iV7RInSQu9YF8s8KbGsZKrFy8MbGBY-kInWdv5dRGMtSHkPd14Fg8FZGaJAaF1HH9w6DyQ'
-
+var test = {
+  endpoint: 'https://sandbox-api.uber.com/v1/requests',
+  redirectUri: 'http://localhost:3000/auth/uber',
+  access_token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZXMiOlsicmVxdWVzdCJdLCJzdWIiOiJmNTcxODY1Ny0zMWYxLTQ1OTctYTk4ZC0zNmI4NGQwOGU3NzkiLCJpc3MiOiJ1YmVyLXVzMSIsImp0aSI6IjI4MjBkYTZjLTcxNWQtNDM1Ni1hNGExLTIyYmNjYWQ1NTgwYiIsImV4cCI6MTQ1ODk0NjU3NCwiaWF0IjoxNDU2MzU0NTc0LCJ1YWN0IjoidUhHQm9RWXJqbUtzSHRVbmtYbFJwbjVrRXhHQTc2IiwibmJmIjoxNDU2MzU0NDg0LCJhdWQiOiJ4OFpCT0dndnZlMkpIUWdPRnVSN2liMmUyZHRfQTY2bSJ9.nBh2WRXLQ-p_hWSMnbWC6jXTOURqZzgwemivQ3YyJrKQLGzhvrOqsbqlWwwOhB2dMco9KwV6JNKoZSskMzvPVdt6Ou15RLnyxYgkHpVsrBb-vgdztvDIWj0VUV55cjqX3KiUNgZwH0ndGDKAvAvFS-OILm_yFegCWnt_CteFXPRuN-S-Q-cE4WZDzMtu7FCDSdPiQC0o83cw9Owf7C_01TnKidpMLY_JTYIaYio_bfdbhQy5MwIttHAbSwrltK8s2lnvBh-qGMnV5Og6iV7RInSQu9YF8s8KbGsZKrFy8MbGBY-kInWdv5dRGMtSHkPd14Fg8FZGaJAaF1HH9w6DyQ'
+}
 
 module.exports = function(userController) {
 
 var Uber = {};
 
 Uber.getToken = function (req, res, next) {
-  request.post("https://login.uber.com/oauth/v2/token?code=" + req.query.code + "&redirect_uri=" + redirectUri + "&client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=authorization_code", function(err, response) {
+  request.post("https://login.uber.com/oauth/v2/token?code=" + req.query.code + "&redirect_uri=" + test.redirectUri + "&client_id=" + config.client_id + "&client_secret=" + config.client_secret + "&grant_type=authorization_code", function(err, response) {
 
     var token = {
       uberToken: JSON.parse(response.body).access_token
@@ -41,9 +43,9 @@ Uber.requestCar = function (req, res, next) {
   }
   
   request.post({ 
-    uri: testEndpoint,
+    uri: test.endpoint,
     headers: {
-      'Authorization': 'Bearer '+testAccessToken
+      'Authorization': 'Bearer '+test.access_token
     },
     body: requestCar,
     json: true
@@ -54,9 +56,9 @@ Uber.requestCar = function (req, res, next) {
 
 Uber.checkStatus = function (req, res, next) {
   request.get({ 
-    uri: testEndpoint+'/<REQUEST_ID>',
+    uri: test.endpoint+'/<REQUEST_ID>',
     headers: {
-      'Authorization': 'Bearer '+testAccessToken
+      'Authorization': 'Bearer '+test.access_token
     }
   } , function(err, response) {
     console.log('Status of uber ride', err, response.body);
@@ -65,9 +67,9 @@ Uber.checkStatus = function (req, res, next) {
 
 Uber.cancelRequest = function (req, res, next) {
   request.delete({ 
-    uri: testEndpoint+'/<REQUEST_ID>',
+    uri: test.endpoint+'/<REQUEST_ID>',
     headers: {
-      'Authorization': 'Bearer '+testAccessToken
+      'Authorization': 'Bearer '+test.access_token
     }
   } , function(err, response) {
     console.log('Cancelling uber request', err, response.body);
