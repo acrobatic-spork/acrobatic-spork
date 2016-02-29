24b1625f75507f5618f879852e968049114bfa6d
shivg <docshivaguru@gmail.com>
2016-02-29 12:16:04 -0800

    Fixes react interval for loading component

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -3,21 +3,26 @@ import ReactInterval from 'react-interval';
 import { browserHistory, Router, Route, Link } from 'react-router'
 
 
-const Loading = (props) => (
-	<div>
-	<div className='loading'>
-	<img src="images/wc-spork.png" />
-	</div>
+const Loading = (props) => {
+	if (!props.store.status) const options = {enabled : true}
+	return	(
+		<div>
+		<div className='loading'>
+		<img src="images/wc-spork.png" />
+		</div>
 
-	<ReactInterval timeout={1000} enabled={true}
-	    callback={() => {
-	      console.log("Loading..");
-		  if (props.store.status !== undefined) {
-		  	browserHistory.push('/status') 
-		  }
-	    }
-	  } />
-	  </div>
-	)
+		<ReactInterval timeout={1000} enabled={options.enabled}
+		    callback={() => {
+		      console.log("Loading..");
+			  if (props.store.status !== undefined) {
+			  	options.enabled = false;
+			  	browserHistory.push('/status');
+			  }
+		    }
+		  } />
+		  </div>
+		)
+	
+}
 
 export default Loading;
