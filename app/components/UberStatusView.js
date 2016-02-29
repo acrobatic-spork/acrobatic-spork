53a8379e37de579de68aaaa9e402630d3a35cf08
Silverhawk <sondra.silverhawk@hush.com>
2016-02-29 14:50:57 -0800

    Reset ride status, stop interval, and route user when ride is cancelled

--- app/components/UberStatusView.js
+++ app/components/UberStatusView.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactInterval from 'react-interval';
+import { browserHistory } from 'react-router';
 
 
 class UberStatus extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      checkStatus: true
+    };
   }
 
   handleCancel (e) {
     e.preventDefault();
     this.props.cancelRide(this.props.status.id);
+    this.setState({
+      checkStatus: false
+    });
+    browserHistory.push('/');
   }
 
   render () {
@@ -21,7 +30,7 @@ class UberStatus extends React.Component {
         </div>
         <div className="uber-status"> 
 
-          <ReactInterval timeout={5000} enabled={true}
+          <ReactInterval timeout={5000} enabled={this.state.checkStatus}
               callback={() => {
                 console.log("Tick");
                 console.log(this.props.status.id);
